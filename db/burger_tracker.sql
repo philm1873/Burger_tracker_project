38354f33ac910ad5d79bfe9b8c879e91cf5096d0
Phil Mitchell <pm1873@gmail.com>
2017-11-26 16:02:10 +0000

    Initial commit

---
+++ db/burger_tracker.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS deals;
+DROP TABLE IF EXISTS burgers;
+DROP TABLE IF EXISTS eateries;
+
+CREATE TABLE eateries(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  address VARCHAR(255),
+  tel_no INT,
+  motto VARCHAR(255),
+  logo VARCHAR(255)
+);
+
+CREATE TABLE burgers(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  type VARCHAR(255),
+  price FLOAT(2),
+  image VARCHAR(255)
+);
+
+CREATE TABLE deals(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  discount FLOAT(2),
+  day VARCHAR(255),
+  eatery_id INT4 REFERENCES eateries(id) ON DELETE CASCADE,
+  burger_id INT4 REFERENCES burgers(id) ON DELETE CASCADE
+);
