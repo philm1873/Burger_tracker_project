73666876bfffa2023330d942545bf59295883886
Phil Mitchell <pm1873@gmail.com>
2017-11-26 18:16:04 +0000

    Added specs files, completed eatery methods

--- models/eatery.rb
+++ models/eatery.rb
@@ -1,3 +1,6 @@
+require_relative('./burger.rb')
+require_relative('../db/sql_runner.rb')
+
 class Eatery
 
   def initialize(input)
@@ -6,7 +9,61 @@ class Eatery
     @address = input['input']
     @tel_no = input['tel_no']
     @motto = input['motto']
-    @logo = input['logo']    
+    @logo = input['logo']
   end
+
+  def save
+    sql = "INSERT INTO eateries(name, address, tel_no, motto, logo)
+    VALUES($1, $2, $3, $4, $5)
+    RETURNING id"
+    values = [@name, @address, @tel_no, @motto, @logo]
+    result = SqlRunner.run(sql, values)
+    @id = result[0]['id']
+  end
+
+  def update
+    sql = "UPDATE eateries
+    SET (name, address, tel_no, motto, logo) = ($1, $2, $3, $4, $5)
+    WHERE id = $6"
+    values = [@name, @address, @tel_no, @motto, @logo, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete
+    sql = "DELETE FROM eateries
+    WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def find_burgers
+    sql = "SELECT *.b FROM deals d
+    INNER JOIN burgers b
+    ON d.burger_id = b.id
+    WHERE d.eatery_id = $1"
+    values = [@id]
+    result = SqlRunner.run(sql, values)
+    return result.map { |burger| Burger.new(burger) }
+  end
+
+  def find_deals
+    sql = "SELECT * FROM deals
+    WHERE eatery_id = $1"
+    values = [@id]
+    result = SqlRunner.run(sql, values)
+    return result.map { |deal| Deal.new(deal) }
+  end
+
+  def self.find_all
+    sql = "SELECT * FROM eateries"
+    result = SqlRunner.run(sql)
+    return result.map { |eatery| Eatery.new(eatery) }
+  end
+
+  def self.delete_all
+    sql = "Delete from eateries"
+    SqlRunner.run(sql)
+  end
+
 
 end
