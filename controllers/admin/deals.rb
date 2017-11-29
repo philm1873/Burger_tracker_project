6c223eed8e05f80e5ca070903b96ee3fe23a6d75
Phil Mitchell <pm1873@gmail.com>
2017-11-29 20:36:33 +0000

    Added CSS

--- controllers/admin/deals.rb
+++ controllers/admin/deals.rb
@@ -14,12 +14,14 @@ get '/admin/deals/:id/edit' do
   @eateries = Eatery.find_all
   @burgers = Burger.find_all
   @deal = Deal.find(params['id'])
+  @days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
   erb(:"admin/deals/edit")
 end
 
 get '/admin/deals/new' do
   @eateries = Eatery.find_all
   @burgers = Burger.find_all
+  @days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
   erb(:"admin/deals/new")
 end
 
